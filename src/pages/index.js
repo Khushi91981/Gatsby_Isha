4349dc7aea7be5e721f842f34e9b2db8a9288091
Isha Sharma <ishasharma91981@gmail.com>
2022-08-31 16:13:23 +0530

    Changes Done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,73 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/layout"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export const query = graphql`
+  query {
+    wpgraphql {
+      pages {
+        nodes {
+          id
+          title
+          uri
+          
+        }
+      }
+      posts {
+        nodes {
+          id
+          title
+          uri
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+const Index = ({ data }) => {
+  const pages = data.wpgraphql.pages.nodes
+  const posts = data.wpgraphql.posts.nodes
+
+  return (
+    <Layout>
+      <h2>List of Pages and Blogs in WordPress Sites</h2>
+      <h3>List of Pages in WordPress Sites</h3>
+      {pages.map(page => (
+        <article key={page.id}>
+            <ul>
+              <li>
+                <Link
+                  to={page.uri}
+                  className="home-links"
+                  dangerouslySetInnerHTML={{ __html: page.title }}
+                />
+              </li>
+            </ul>
+          
+        </article>
+      ))}
+       <h3>List of Blogs in WordPress Sites</h3>
+     
+       {posts.map(post => (
+        
+        <article key={post.id}>
+          <ul>
+            <li>
+            <Link
+              to={`/blog/${post.uri}`}
+              className="home-links"
+              dangerouslySetInnerHTML={{ __html: post.title }}
+            />
+            </li>
+          </ul>
+          
+        </article>
+        
+      ))}
+    
+    </Layout>
+  )
 }
+
+export default Index
